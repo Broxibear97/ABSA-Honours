102968e1d3691a68887cc62a57c87b7c94b05cd3
Joshua Hogarth <JHogarth@dundee.ac.uk>
2018-02-05 02:32:01 +0000

    Initial Python Program

---
+++ Program.py
@@ -0,0 +1,5 @@
+import nltk
+import praw
+
+reddit = praw.Reddit(client_id="7vOWHznFDrqBVA", client_secret="ExRrk05OkZat5VAYNIARhoQVS0o",
+                     user_agent="windows:UniHonoursApp:v1.0.0 (by /u/Delacarpet)")
