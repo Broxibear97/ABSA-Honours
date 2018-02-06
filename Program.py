e3177b36573e6e30918b1a5501fe37a8aab7d643
Joshua Hogarth <JHogarth@dundee.ac.uk>
2018-02-06 21:30:14 +0000

    Got comments to display from reddit.
    Shows comments as well as reply depth

--- Program.py
+++ Program.py
@@ -1,5 +1,23 @@
-import nltk
 import praw
+from praw.models import MoreComments
+
+
+def display_all_comments(comments, level):
+    print((" " * level) + (">" * level) + comments.body)
+    for commentReply in comments.replies:
+        display_all_comments(commentReply, level + 1)
+        print()
+
 
 reddit = praw.Reddit(client_id="7vOWHznFDrqBVA", client_secret="ExRrk05OkZat5VAYNIARhoQVS0o",
                      user_agent="windows:UniHonoursApp:v1.0.0 (by /u/Delacarpet)")
+
+submissionLink = input("Enter Reddit URL to perform ABSA\n")
+submission = reddit.submission(url=submissionLink)
+
+submission.comments.replace_more(limit=None)
+
+for comment in submission.comments:
+    display_all_comments(comment, 0)
+
+
